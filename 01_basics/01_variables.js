1546d77f8b9d2af0e4d92e60f6758cd375254695
sumansaurabh1325-svg <sumansaurabh1325@gmail.com>
2026-09-09 16:50:03 +0000

    dataTypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 12345
+let accountEmail = "suman@google.com"
+var accountPassword = "67890"
+let accountState
+
+//accountId = 2
+
+accountEmail = "saurabh@go.com"
+accountPassword = "4122"
+
+console.log(accountId);
+console.table([accountId, accountEmail , accountPassword, accountState])
+
+/*
+Perfer not to  use var in the javascript 
+becase of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version 
+
+// alert(3 + 3) // we are using nodjs , not browser
+
+console.log(3+3)  // code redability should be high
+
+console.log("Suman")
+
+let name="suman"
+let age = 22
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => true/false
+// boolean => true/false
+// null => standalone value
+// undefined =>  (means value is not assign yet!)
+// symbole => unique
+
+
+//object
+
+console.log(typeof undefined) // undefined  datatype only
+console.log(typeof null)  // Object  datatype
