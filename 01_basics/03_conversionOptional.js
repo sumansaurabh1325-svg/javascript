7daf617da8b672434c3d920e8ff0b042600ca640
sumansaurabh1325-svg <sumansaurabh1325@gmail.com>
2026-09-11 18:34:19 +0000

    conversion

--- 01_basics/03_conversionOptional.js
+++ 01_basics/03_conversionOptional.js
@@ -22,5 +22,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber )
+//console.log(stringNumber)
+//console.log(typeof stringNumber )
+
+
+//******************* Operations  ******************
+let value = 3
+let negValue = -value
+//console.log(negValue)
+ 
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(2/2)
+//console.log(2**2)
+//console.log(2%2)
+
+let str1 = "hello"
+let str2 = " suman"
+let str3 = str1+str2
+//console.log(str3)   // hello suman
+
+//console.log("1" + 2); // if string is frist then after that evey thing will considered as string
+//console.log(1 + "2";)
+//console.log("1"+2+2); // string frist means evthing after is string
+//console.log(1+2+"2"); // but in this case number first then string
+
+
+//console.log((3 + 4) * 5 % 3);
+
+//console.log(+true)
+//console.log(+"")
+
+let gameCounter = 100
+gameCounter++ //postfix sum
+//++gameCounter //prefixsum
+console.log(gameCounter)
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
